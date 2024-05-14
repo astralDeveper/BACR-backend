44d1bebc3fc9b0c00fe741e2d4b2043c6bfc6819
astralDeveper <157403596+astralDeveper@users.noreply.github.com>
2024-05-15 02:10:31 +0500

    Updated server.listen function

--- index.js
+++ index.js
@@ -5,42 +5,18 @@ const bodyparser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
 const server = http.createServer(app);
-const Maincontroller = require("./controllers/main");
-const Authcontroller = require("./controllers/auth");
-const mongoose = require("mongoose");
-require("dotenv").config();
-// const dbconnection = require("./utliz/dbConnection");
+const controller = require("./controllers");
+const dbconnection = require("./utliz/dbConnection");
 
-app.use(cors({ origin: "*" }));
-app.use(bodyparser.urlencoded({ extended: false }));
+app.use(cors({origin:"*"}));
+app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 app.use(morgan("tiny"));
 
-app.use(Maincontroller);
-app.use(Authcontroller);
+app.use(controller);
 
-const start = async () => {
-  try {
-    if (mongoose.connection.readyState !== 1) {
-      await mongoose.connect(process.env.MONGODB_URI);
-      console.log("connected to Database...");
-      server.listen(3001);
-      server.on("listening", () => {
-        console.log("server is up...");
-      });
-
-      return;
-    }
-    console.log("alredy connected to Database...");
-    server.listen(3000);
-    server.on("listening", () => {
-      console.log("server is up...");
-    });
-    return;
-  } catch (error) {
-    console.log(error.message);
-    process.exit(1);
-  }
-};
-
-start();
+server.listen(3000);
+server.on("listening", ()=>{
+    console.log("server is up...");
+    dbconnection();
+})
