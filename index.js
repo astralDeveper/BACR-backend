ee03f08f159b858ced4e78f599f12a84015390b7
astralDeveper <157403596+astralDeveper@users.noreply.github.com>
2024-05-15 02:15:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const http = require("http");
 const cors = require("cors");
 const bodyparser = require("body-parser");
 const morgan = require("morgan");
-const app = express();
-const server = http.createServer(app);
 const controller = require("./controllers/auth.js");
 const dbconnection = require("./utliz/dbConnection.js");
+const app = express();
 
 app.use(cors({origin:"*"}));
 app.use(bodyparser.urlencoded({extended:false}));
@@ -15,8 +13,8 @@ app.use(morgan("tiny"));
 
 app.use(controller);
 
-server.listen(3000);
-server.on("listening", ()=>{
+app.listen(3000);
+app.on("listening", ()=>{
     console.log("server is up...");
     dbconnection();
 })
